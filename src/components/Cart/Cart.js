974b2e1d115511f6e491f5131fb2c09c9a9bd5e5
dragontea72 <riyadcseiiuc72@gmail.com>
2020-08-25 04:46:42 +0600

    done ema-jhon site

---
+++ src/components/Cart/Cart.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const Cart = (props) => {
+    const cart = props.cart;
+   // const totalPrice = cart.reduce((total, prd) => total + prd.price, 0);
+   let total = 0;
+   for(let i = 0;i<cart.length;i++){
+       const product = cart[i];
+       total = total + product.price;
+   }
+
+   let shipping = 0;
+   if(total > 35){
+       shipping = 0;
+   }else if(total >15){
+       shipping=4.99;
+   }else if(total >0){
+       shipping = 12.9
+    }
+    
+    const tax = Math.round(total/10);
+    return (
+        <div>
+            <h4>Order summary</h4>
+            <p>Items Ordered:{cart.length}</p>
+             <p>Product Price: {total}</p>
+            <p><small>Shipping Cost: {shipping}</small></p>
+            <p><small>Tax + Vat: {tax}</small></p>
+            <p>Total Price: {total + shipping}</p>
+            
+        </div>
+    );
+};
+
+export default Cart;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from '../../images/logo.png'
+import './Header.css';
+
+
+
+const Header = () => {
+    return (
+        <div className="header">
+            <img src={logo} alt=""></img>
+             <nav>
+                 <a href="/shop">Shop</a>
+                 <a href="/review">Order Review</a>
+                 <a href="/manage">Manage Invent</a>
+             </nav>
+        </div>
+    );
+};
+
+export default Header;
